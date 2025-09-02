8edc316ad1cebbb290f11c8f17a7180964da925b
Eduardo Alfonso <140914423+EduardoAlfonsoDev@users.noreply.github.com>
2025-09-01 21:54:05 -0300

    Add files via upload

---
+++ algoritmo.js
@@ -0,0 +1,19 @@
+// Fazendo café 
+// primeiroPasso = "1. Coloque água na chaleira \n"
+// segundoPasso = "2. Aqueça até ferver \n"
+//terceiroPasso = "3. Coloque café no filtro \n"
+//quartoPasso = "4. Despeje água quente no café \n"
+// quintoPasso = "5. Sirva \n"
+
+//utilizando o console.log oara exibir as informações 
+// console.log()
+
+// fazendo chimarrão
+
+primeiroPassso = "Esquente a água \n"
+segundoPasso = "Pegue a cuia, bomba e a erva mate \n"
+terceiroPasso = "coloque a água quente na cuia \n"
+quartoPasso = "coloque a erva mate por cima da água \n"
+quintoPasso = "aproveite o chimarrão"
+
+console.log(primeiroPassso,segundoPasso,terceiroPasso,quartoPasso,quintoPasso)

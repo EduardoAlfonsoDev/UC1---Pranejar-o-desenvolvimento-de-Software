40cca4a79eab1820bdf11fcefc380d479cb5500d
Eduardo Alfonso <140914423+EduardoAlfonsoDev@users.noreply.github.com>
2025-09-02 19:54:33 -0300

    Add files via upload

---
+++ bolo2.js
@@ -0,0 +1,17 @@
+// bolo simples
+
+// sabores: chocolate, baunilha e morango
+
+// preparo da massa
+
+passoUm = "1. bata os ovos, açúcar e óleo até ficar fofo. \n"
+passoDois = "2. acrescente o leite e misture. \n"
+passoTres = "3. coloque a farinha peneirada e misture delicadamente. \n"
+passoQuatro = "4. divida em 3 tigelas: Na primeira, adicione o chocolate. Na segunda, a baunilha. Na terceira o morango. \n"
+passoCinco = "5. Misture cada massa e asse separadamente em formas do mesmo tamanho (180°C por 30-35 minutos). \n"
+parte1 = "6. coloque a massa de baunilha como base. \n"
+parte2 = "7. adicione a massa de morango. \n "
+parte3 = "8. finalize com a massa de chocolate . \n"
+parte4 = "9. cubra com chantilly, ganache ou glacê a gosto. \n" 
+
+console.log(passoUm,passoDois,passoTres,passoQuatro,passoCinco,parte1,parte2,parte3,parte4)
